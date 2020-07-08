077dda91d5a4ec384f837de8ca2d7131c7fd5302
grey_wolf <grey_wolf.88@mail.ru>
2020-07-08 20:35:01 +0300

    Добавлен интерфейс IDisposable

--- WpfApp3/ViewModels/Base/ViewModel.cs
+++ WpfApp3/ViewModels/Base/ViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace WpfApp3.ViewModels.Base
 {
-    internal abstract class ViewModel: INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -21,6 +21,19 @@ namespace WpfApp3.ViewModels.Base
             field = value;
             OnProperyChanged(PropertyName);
             return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
         }
     }
 }
