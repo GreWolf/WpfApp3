da3fe9ab59035148f717823d2ed2479731046668
grey_wolf <grey_wolf.88@mail.ru>
2020-07-08 20:24:57 +0300

    Базовый класс модели представления

---
+++ WpfApp3/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfApp3.ViewModels.Base
+{
+    internal abstract class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnProperyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnProperyChanged(PropertyName);
+            return true;
+        }
+    }
+}
