fb70d14281d67bd6fbdadc815db36666ae6e136f
grey_wolf <grey_wolf.88@mail.ru>
2020-07-08 18:13:17 +0300

    Тестовый консольный проект

---
+++ Tests/WpfApp3Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WpfApp3Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
