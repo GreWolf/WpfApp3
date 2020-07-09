fa0e0c6518e92ec9f05f14738d6441ae04ccb0fc
grey_wolf <grey_wolf.88@mail.ru>
2020-07-09 06:36:17 +0300

    Тестовая разметка

--- WpfApp3/ViewModels/MainWindowViewModel.cs
+++ WpfApp3/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WpfApp3.ViewModels.Base;
 
 namespace WpfApp3.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Заголово окна";
+
+        /// <summary>Заголовок окна</summary>
+
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnProperyChanged();
+
+            //    Set(ref _Title, value);
+
+            //}
+
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
