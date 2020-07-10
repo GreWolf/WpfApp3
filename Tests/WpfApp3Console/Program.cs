d541c27563a07b95a202ab9f2fa0226f0cc53098
grey_wolf <grey_wolf.88@mail.ru>
2020-07-10 20:24:56 +0300

    Извлечение данных по датам

--- Tests/WpfApp3Console/Program.cs
+++ Tests/WpfApp3Console/Program.cs
@@ -1,5 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Globalization;
 
 namespace WpfApp3Console
 {
@@ -7,14 +16,43 @@ namespace WpfApp3Console
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-        static void Main(string[] args)
+        private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
 
-            var response = client.GetAsync(data_url).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
 
-            Console.WriteLine(client);
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+        static void Main(string[] args)
+        {
+
+            //foreach (var data_line in GetDataLines())
+            //    Console.WriteLine(data_line);
+
+            var dates = GetDates();
+            Console.WriteLine(String.Join("\r\n", dates));
+
+            Console.ReadLine();
         }
     }
 }
