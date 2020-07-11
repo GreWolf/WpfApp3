295fd712a08c761ae3d2a082a35412bd0b86323c
grey_wolf <grey_wolf.88@mail.ru>
2020-07-11 15:03:16 +0300

    Тестирование бизнес-логики

--- Tests/WpfApp3Console/Program.cs
+++ Tests/WpfApp3Console/Program.cs
@@ -32,7 +32,7 @@ namespace WpfApp3Console
             {
                 var line = data_reader.ReadLine();
                 if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line;
+                yield return line.Replace("Korea,", "Korea -");
             }
         }
 
@@ -43,14 +43,34 @@ namespace WpfApp3Console
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
 
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+
+            foreach (var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+
+                yield return (country_name, province, counts);
+            }
+        }
         static void Main(string[] args)
         {
 
             //foreach (var data_line in GetDataLines())
             //    Console.WriteLine(data_line);
 
-            var dates = GetDates();
-            Console.WriteLine(String.Join("\r\n", dates));
+            //var dates = GetDates();
+            //Console.WriteLine(String.Join("\r\n", dates));
+
+            var russia_data = GetData()
+                .First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count)=> $"{date:dd:MM:yy} - {count}")));
 
             Console.ReadLine();
         }
